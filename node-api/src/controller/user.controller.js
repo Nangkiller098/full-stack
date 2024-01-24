7c72bc53f31e53c141c3b5fe96b6abb741b7798a
Nangkiller098 <hsomnang098@gmail.com>
2024-01-24 22:45:32 +0700

    CRUD user

--- node-api/src/controller/user.controller.js
+++ node-api/src/controller/user.controller.js
@@ -45,10 +45,40 @@ const create = async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 };
-const update = (req, res) => {
-  res.send("Your have Request Update User");
+const update = async (req, res) => {
+  try {
+    var sql =
+      "UPDATE ROLE SET Name= :Name,Code= :Code,Status= :Status WHERE Id= :Id";
+    var param = {
+      Id: req.body.Id,
+      Name: req.body.Name,
+      Code: req.body.Code,
+      Status: req.body.Status,
+    };
+    const [data] = await db.query(sql, param);
+    res.json({
+      message: data.affectedRows != 0 ? "Update success" : "Not Found",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+    console.log(error);
+  }
 };
-const remove = (req, res) => {
-  res.send("Yor have request Delte User");
+const remove = async (req, res) => {
+  try {
+    var sql = "DELETE FROM role WHERE Id= :Id";
+    var param = {
+      Id: req.body.Id,
+    };
+    const [data] = await db.query(sql, param);
+    res.json({
+      message: "Remove success",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+    console.log(error);
+  }
 };
 module.exports = { getList, create, update, remove };
