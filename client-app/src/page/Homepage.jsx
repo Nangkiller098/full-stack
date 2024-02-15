d3102fd78d276d32cee24a4bc395f8889f1d853e
heng somnang <hsomnang098@gmail.com>
2024-02-15 09:59:57 +0700

    basice react

--- client-app/src/page/Homepage.jsx
+++ client-app/src/page/Homepage.jsx
@@ -1,5 +1,66 @@
-const Homepage = () => {
-  return <div>Homepage</div>;
+import { useEffect, useState } from "react";
+import axios from "axios";
+const HomePage = () => {
+  const [list, setList] = useState([]);
+  const [message, setMessage] = useState("");
+  const [role, setRole] = useState("");
+  const [a, setA] = useState(undefined);
+
+  useEffect(() => {
+    getListCategory();
+  }, []);
+
+  const getListCategory = () => {
+    axios({
+      url: "http://localhost:8081/api/customer/getlist",
+      method: "get",
+      data: {
+        // Fistname:"Dara",
+        // Gender:"Dara",
+      },
+    })
+      .then((res) => {
+        console.log(res.data);
+        setList(res.data.list);
+        setMessage(res.data.message);
+        setRole(res.data.role); //
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <h1>message : {message}</h1>
+      <h1>role : {role + ""}</h1>
+      <h1>List : {list.length}</h1>
+      {a && <h1>a : {a.name}</h1>}
+      <h1>a : {a?.name}</h1>
+      {list.length > 0 && (
+        <h1>
+          Username : {list[0].Firstname}-{list[0].Lastname}
+        </h1>
+      )}
+      <div>
+        {list.map((item, index) => (
+          <>
+            <div
+              style={{ padding: 10, backgroundColor: "green", marginTop: 10 }}
+            >
+              <div>
+                {index + 1}. {item.Firstname}-{item.Lastname}
+              </div>
+              <div>Gender : {item.Gender}</div>
+              <div>Contact : {item.Tel}</div>
+              <div>Email : {item.Email}</div>
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Homepage;
+export default HomePage;
--- node-api/index.js
+++ node-api/index.js
@@ -1,6 +1,12 @@
 const express = require(`express`);
 const app = express();
+const cors = require("cors");
 app.use(express.json());
+
+app.use(cors({
+  "origin":"*",
+  "methods":"GET,HEAD,PUT,PATCH,POST,DELETE"
+}));
 
 app.get("/", (req, res) => {
   res.send("Hello world");
